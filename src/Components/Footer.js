7b7d8656e305fcdfa1277199bf7ad5f94709ed43
Harrison Lee <laharrari@gmail.com>
2020-11-17 04:39:56 -0800

    Added Header and Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="bg-black text-white text-center p-3 absolute bottom-0 w-full">
+            <div className="flex justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-12">
+                <a href="http://www.laharrari.com/" target="_blank" rel="noreferrer">Website</a>
+                <a href="https://www.linkedin.com/in/laharrari/" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="https://github.com/laharrari/" target="_blank" rel="noreferrer">GitHub</a>
+            </div>
+
+            <div>
+                Email: laharrari@gmail.com
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="bg-black text-white text-center text-2xl p-3 text-center font-bold">
+            Rodeo Cinema
+        </header>
+    )
+}
+
+export default Header;
