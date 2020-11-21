b4b28ffeb5a8556e0fd214a4ee43c6fe4c4f567b
Harrison Lee <laharrari@gmail.com>
2020-11-21 05:41:12 -0800

    Centered lobby image

--- client/src/Components/Lobby.js
+++ client/src/Components/Lobby.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import pic from '../images/rodeo_cinema.png';
+import pic from '../Images/rodeo_cinema.png';
 
 function Lobby() {
     const [name, setName] = useState('');
@@ -10,11 +10,15 @@ function Lobby() {
         <div className="flex justify-center items-center">
             <div>
                 <h1 className="mt-10 text-discordTextMain text-2xl text-center font-bold">Lobby</h1>
-                <div><input placeholder="Name" type="text" className="pl-1 mt-5 w-56 outline-none"
-                onChange={(event) => setName(event.target.value)} /></div>
+                <div className="flex justify-center items-center">
+                    <input placeholder="Name" type="text" className="pl-1 mt-5 w-56 outline-none"
+                onChange={(event) => setName(event.target.value)} />
+            </div>
 
-                <div><input placeholder="Room" type="text" className="pl-1 mt-5 w-56 outline-none"
-                onChange={(event) => setTheater(event.target.value)} /></div>
+                <div className="flex justify-center items-center">
+                    <input placeholder="Room" type="text" className="pl-1 mt-5 w-56 outline-none"
+                    onChange={(event) => setTheater(event.target.value)} />
+                </div>
 
                 <div className="flex justify-center items-center">
                     <Link onClick={event => (!name || !theater) ? event.preventDefault() : null}
@@ -22,7 +26,10 @@ function Lobby() {
                         <button className="bg-discordMain text-discordTextMain text-xl mt-5 px-10 rounded" type="submit">Join</button>
                     </Link>
                 </div>
-                {/* <img className="w-3/4 h-3/4" src={pic} alt="rodeo_cinema.png" /> */}
+                
+                <div className="flex justify-center items-center">
+                    <img className="w-3/4 h-3/4 p-10" src={pic} alt="rodeo_cinema.png" />
+                </div>
             </div>
         </div>
     )
